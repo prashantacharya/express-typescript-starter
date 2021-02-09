110057501f785be81dd450cca688e96c38c80bb8
Prashant Acharya <prashantacharya@lftechnology.com>
2021-02-09 19:57:38 +0545

    Install necessary dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      1,
+      { semi: true, tabWidth: 2, useTabs: false, singleQuote: true },
+    ],
+    ...require('eslint-config-prettier').rules,
+    ...require('eslint-config-prettier/@typescript-eslint').rules,
+  },
+};
